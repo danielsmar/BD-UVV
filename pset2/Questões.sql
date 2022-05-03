3b9b8f7cf8587db9d218a6655a9d81ebb52c891a
Daniel Marculano <91577318+danielsmar@users.noreply.github.com>
2022-05-02 21:52:05 -0300

    Add files via upload

---
+++ pset2/Questões.sql
@@ -0,0 +1,49 @@
+PSET 2
+
+questao 1
+SELECT numero_departamento, AVG(salario) AS media
+FROM funcionario 
+GROUP BY numero_departamento
+order BY numero_departamento ;
+
+questao 2
+SELECT sexo, AVG(salario) AS media
+FROM funcionario 
+GROUP BY sexo
+ORDER BY sexo ;
+
+questao 3
+select
+	f.primeiro_nome||' '||f.nome_meio ||' '||f.ultimo_nome,
+	f.data_nascimento,
+	DATE_PART('year', current_date) - DATE_PART('year', f.data_nascimento) as idade,
+	f.salario,
+	d.nome_departamento
+from
+	funcionario f
+
+join departamento d on f.numero_departamento = d.numero_departamento 
+order by nome_departamento 
+
+questão 4
+select
+	f.primeiro_nome||' '||f.nome_meio ||' '||f.ultimo_nome as nome_completo,
+	DATE_PART('year', current_date) - DATE_PART('year', f.data_nascimento) as idade,
+	f.salario as salario_atual,
+	(case when (f.salario < '35000') then f.salario*1.2
+		  when (f.salario >= '35000') then f.salario*1.15 end ) as novo_salario
+from funcionario f 
+
+
+--questao 05
+select
+	f.primeiro_nome || ' ' || f.nome_meio || ' ' || f.ultimo_nome as nome_completo,
+	f.salario,
+	d.nome_departamento
+	
+	
+from
+	funcionario f
+
+join departamento d on f.numero_departamento = d.numero_departamento 
+order by nome_departamento  asc,salario desc
